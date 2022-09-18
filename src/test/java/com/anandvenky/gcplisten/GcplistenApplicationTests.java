d9d541f746d56b5f43e05b2e80b85e0ed9757e98
cgmafia <anandvenkatarama@gmail.com>
2022-09-18 09:08:55 +0200

    init google cloud pubsub listener basecode

---
+++ src/test/java/com/anandvenky/gcplisten/GcplistenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anandvenky.gcplisten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcplistenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
